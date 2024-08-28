7f75870e99c591df80680856a0f1eeff566877e1
Zaeem2678 <102031010+Zaeem2678@users.noreply.github.com>
2024-08-28 23:15:31 +0500

    new file added
    test2 . java

---
+++ Test2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test2;
+
+/**
+ *
+ * @author Micro Tech
+ */
+public class Test2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+        System.out.println("Sum is "+sum);
+    }
+    
+}
